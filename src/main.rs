e61616d0b415832ee5f2126af72f5317290caba3
Tom Draper <tomjdraper1@gmail.com>
2023-01-05 14:13:31 +0000

    Interpolation

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ mod nlm;
 mod export;
 mod array;
 mod operation;
+use std::time::Instant;
+
 use crate::export::{write_to_csv};
 
 fn main() {
+    let start = Instant::now();
     let arr = nlm::random_element(200, 200, 100000);
+    let elapsed = start.elapsed();
+    println!("elapsed: {:?}", elapsed);
     write_to_csv(arr);
 }
--- src/operation.rs
+++ src/operation.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 
 pub fn max(vec: &Vec<Vec<f32>>) -> f32 {
     let mut max: f32 = 0.0;
@@ -23,8 +25,29 @@ pub fn min(vec: &Vec<Vec<f32>>) -> f32 {
     min
 }
 
+fn nearest_neighbour(vec: &Vec<Vec<f32>>, row: usize, col: usize) -> f32 {
+    let mut rng = rand::thread_rng();
+    let n = rng.gen_range(0..4);
+    if n == 0 {
+        return vec[row+1][col];
+    } else if n == 1 {
+        return vec[row-1][col];
+    } else if n == 2 {
+        return vec[row][col+1];
+    } else  {
+        return vec[row][col-1];
+    }
+}
+
 pub fn interpolate(vec: &mut Vec<Vec<f32>>, mask: Vec<Vec<bool>>) {
-    
+    for i in 0..vec.len() {
+        for j in 0..vec[i].len() {
+            if mask[i][j] {
+                // Replace with nearest neighbour
+                vec[i][j] = nearest_neighbour(vec, i, j);
+            }
+        }
+    }
 }
 
 pub fn scale(vec: &mut Vec<Vec<f32>>) {
