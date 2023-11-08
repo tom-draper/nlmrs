9f6bae3cdd10600cd673b1ff5a08395e278231f5
Tom Draper <41476809+tom-draper@users.noreply.github.com>
2023-11-08 15:01:16 +0000

    Update export.rs

--- src/export.rs
+++ src/export.rs
@@ -1,3 +1,7 @@
+use serde_json;
+use std::fs::File;
+use std::io::{BufWriter, Write, Result};
+
 use csv::Writer;
 
 pub fn display_arr(arr: Vec<Vec<f64>>) {
@@ -6,9 +10,18 @@ pub fn display_arr(arr: Vec<Vec<f64>>) {
     }
 }
 
-pub fn write_to_csv(arr: Vec<Vec<f64>>) {
+pub fn write_to_csv(arr: Vec<Vec<f64>>) -> Result<()> {
     let mut wtr = Writer::from_path("data/data.csv").unwrap();
     for row in arr.iter() {
         let _ = wtr.serialize(row);
     }
+    Ok(())
+}
+
+pub fn write_to_json(arr: Vec<Vec<f64>>) -> Result<()> {
+    let file = File::create("data/data.json")?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &arr)?;
+    writer.flush()?;
+    Ok(())
 }
